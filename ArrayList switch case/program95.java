03fc780ed62ddbf36982e3a94c3a81c167b0f98d
shashikant singh <codershashi322@gmail.com>
2024-04-20 20:24:44 +0530

    100DaysCodeChallenge Day: 21/100

---
+++ ArrayList switch case/program95.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)  throws java.lang.Exception{
+        //Switch Case Problem 1
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt(); 
+         switch(N)
+             {
+                 case 55:
+                     System.out.println( "Study is important");
+                     break;
+                 default:
+                     System.out.println( "No value");
+             }
+    }
+}
