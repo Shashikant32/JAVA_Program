d1cc709db4ecdc7791d64abb0f21391da246f47d
shashikant singh <codershashi322@gmail.com>
2024-04-20 20:24:36 +0530

    100DaysCodeChallenge Day: 21/100

---
+++ ArrayList switch case/program94.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws Throwable{
+        //Switch Case Problem 2
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        switch(N)
+             {
+                 case 12:
+                     System.out.println(  "I am even");
+                     break;
+                case 13:
+                     System.out.println(  "I am prime");
+                     break;
+                 case 25:
+                     System.out.println(  "I am odd");
+                     break;    
+                 default:
+                     System.out.println( "I am just a number");
+             }
+    }
+}
