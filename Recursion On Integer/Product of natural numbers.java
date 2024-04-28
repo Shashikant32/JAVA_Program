c0e9b8a34ba187cb5f4f936be0ed733027a839c1
shashikant singh <codershashi322@gmail.com>
2024-04-28 22:22:59 +0530

    100DaysCodeChallenge Day: 29/100

---
+++ Recursion On Integer/Product of natural numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+	static long product(int n){
+		//Write code here
+        if(n==1)
+        {
+            return 1;
+        }
+        long fact = n*product(n-1);
+       return fact;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println(product(n));
+    }
+}
