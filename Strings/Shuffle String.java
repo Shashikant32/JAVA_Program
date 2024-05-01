d3dee97f1230a7b94522692e69f8a15098fe51c1
shashikant singh <codershashi322@gmail.com>
2024-05-01 22:13:33 +0530

    Update Shuffle String.java

--- Strings/Shuffle String.java
+++ Strings/Shuffle String.java
@@ -4,18 +4,15 @@ public class Main {
     
     static void shuffleString(int[] indices, String s) {
         char[] shuffledChars = new char[s.length()];
-
         // Place each character from the input string at the corresponding index in the shuffledChars array
         for (int i = 0; i < indices.length; i++) {
             shuffledChars[indices[i]] = s.charAt(i);
         }
-
         // Construct a new string from the shuffled character array
         StringBuilder sb = new StringBuilder();
         for (char ch : shuffledChars) {
             sb.append(ch);
         }
-
         // Print the shuffled string
         System.out.println(sb.toString());
     }    
