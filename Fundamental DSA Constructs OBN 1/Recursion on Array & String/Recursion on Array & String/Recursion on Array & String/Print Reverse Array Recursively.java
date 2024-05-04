22002803eb9f1915d3d5081e5a9e1264ec813dcd
shashikant singh <codershashi322@gmail.com>
2024-05-04 14:05:49 +0530

    100DaysCodeChallenge Day: 35/100

---
+++ Fundamental DSA Constructs OBN 1/Recursion on Array & String/Recursion on Array & String/Recursion on Array & String/Print Reverse Array Recursively.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        PrintReverseArray(arr, n);
+    }
+
+	public static void PrintReverseArray(int[] arr, int n) {
+        if(n==0)
+        {
+            return;
+        }
+        System.out.print(arr[n-1]+" ");
+        PrintReverseArray(arr,n-1);
+    }
+
+  }
