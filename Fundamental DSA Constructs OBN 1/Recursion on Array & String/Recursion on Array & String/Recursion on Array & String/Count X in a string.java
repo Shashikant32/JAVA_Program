a403c130308054f3eadf162ec3b952dea31b3386
shashikant singh <codershashi322@gmail.com>
2024-05-05 21:12:21 +0530

    100DaysCodeChallenge Day: 36/100

---
+++ Fundamental DSA Constructs OBN 1/Recursion on Array & String/Recursion on Array & String/Recursion on Array & String/Count X in a string.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    static int countX(String str, int i, int n) {
+        if(i==n)
+        {
+            return 0;
+        }
+        if(str.charAt(i)== 'x')
+        {
+            return 1+ countX(str,i+1,n);
+        }
+        return countX(str,i+1,n);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n=str.length();
+        System.out.println(countX(str,0,n));
+    }
+}
