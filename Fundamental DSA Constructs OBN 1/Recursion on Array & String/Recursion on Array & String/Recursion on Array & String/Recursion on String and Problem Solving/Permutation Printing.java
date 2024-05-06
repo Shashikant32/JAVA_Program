60ae79159f791aa9240213a11bc0289c0df9109a
shashikant singh <codershashi322@gmail.com>
2024-05-06 23:10:46 +0530

    100DaysCodeChallenge Day: 37/100

---
+++ Fundamental DSA Constructs OBN 1/Recursion on Array & String/Recursion on Array & String/Recursion on Array & String/Recursion on String and Problem Solving/Permutation Printing.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Accio {
+    
+    public void printPermutations(String str) {
+        TreeSet<String> ans = new TreeSet<>();
+        helper(str,0,ans);
+        for(String ele:ans)//for each loop
+            {
+                System.out.println(ele);
+            }
+    }
+    static void helper(String str, int i,TreeSet<String> ans)
+    {
+        if(i==str.length())
+        {
+            ans.add(str);
+            return;
+        }
+        for(int j=i; j<str.length(); j++)
+            {
+                str =swap(str,i,j);
+                helper(str,i+1,ans);
+                //str=swap(str,i,j);// backtracking step
+            }
+    }
+    static String swap(String str, int i, int j)
+    {
+        char[] arr = str.toCharArray();
+        char temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        return String.valueOf(arr);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        Accio Obj = new Accio();
+        Obj.printPermutations(str);
+    }
+}
