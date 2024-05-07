844e3706fa9e39955a5f6d7df90f4b15c25cf818
shashikant singh <codershashi322@gmail.com>
2024-05-07 23:33:34 +0530

    100DaysCodeChallenge Day: 38/100

---
+++ Fundamental DSA Constructs OBN 1/Recursion on Array & String/Recursion on Array & String/Recursion on Array & String/Recursion on String and Problem Solving/Find Digit Sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+	static int sumOfDigi(int n) {
+         if (n < 10) {
+            return n;
+        } else {
+            return n % 10 + sumOfDigi(n / 10);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int result = sumOfDigi(n);
+        System.out.println(result);
+        sc.close();
+    }
+}
