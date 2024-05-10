93d62a55ad2da5d54e9e9e23d1abdbc795e35c1f
shashikant singh <codershashi322@gmail.com>
2024-05-10 23:21:33 +0530

    100DaysCodeChallenge Day:41 /100

---
+++ Fundamental DSA Constructs OBN 1/Recursion on Array & String/Recursion on Array & String/Recursion on Array & String/Recursion on String and Problem Solving/No X.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    static String noX(String s) 
+    {
+           if (s.length() == 0) 
+           {
+            return "";
+        }
+
+        char firstChar = s.charAt(0);
+        if (firstChar == 'x') {
+            return noX(s.substring(1));
+        } else {
+            return firstChar + noX(s.substring(1));
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        System.out.println(noX(s));
+    }
+}
