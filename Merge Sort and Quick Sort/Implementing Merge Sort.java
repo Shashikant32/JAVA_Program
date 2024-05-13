acd5a46cd0182a9840cc82d014b2eb19c2c27ad9
shashikant singh <codershashi322@gmail.com>
2024-05-13 23:34:03 +0530

    100DaysCodeChallenge Day:44 /100

---
+++ Merge Sort and Quick Sort/Implementing Merge Sort.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Solution {
+    static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+}
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = input.nextInt();
+        input.close();
+
+        Solution Obj = new Solution();
+        Obj.mergeSort(a, 0, n - 1);
+        for (int i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+    }
+}
