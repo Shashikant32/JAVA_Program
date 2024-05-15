c3dc127e81f60411326fada4b92b3df44a39ca3e
shashikant singh <codershashi322@gmail.com>
2024-05-15 23:14:58 +0530

    100DaysCodeChallenge Day:46 /100

---
+++ LinkedList/Running Time.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Solution s = new Solution();
+        System.out.println(s.countNumberOfInsertionSortInversions(arr));
+    }
+}
+
+class Solution {
+    int count = 0; 
+
+    int countNumberOfInsertionSortInversions(int[] arr) {
+        insertionSort(arr);
+        return count;
+    }
+
+    void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            
+            
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                count++; 
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
