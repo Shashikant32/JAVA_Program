b40e89138bbd4984042a6e9993f05f8f28b00fee
shashikant singh <codershashi322@gmail.com>
2024-05-16 23:41:19 +0530

    Update Max Sum Subarray of size K.java

--- Array/Max Sum Subarray of size K.java
+++ Array/Max Sum Subarray of size K.java
@@ -9,11 +9,9 @@ class Solution {
         for (int i = 0; i < k; i++) {
             windowSum += arr[i];
         }
-
         // Update maxSum with sum of first window
         maxSum = Math.max(maxSum, windowSum);
 
-        
         for (int i = k; i < arr.length; i++) {
             windowSum += arr[i] - arr[i - k];
             maxSum = Math.max(maxSum, windowSum);
