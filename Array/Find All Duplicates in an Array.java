b1b2289aedeea9f08c8633c6aca35cf78508da89
shashikant singh <codershashi322@gmail.com>
2024-05-21 23:32:07 +0530

    100DaysCodeChallenge Day:52 /100

---
+++ Array/Find All Duplicates in an Array.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public void allDuplicates(int n, int[] arr) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(arr[i]) - 1;
+            if (arr[index] < 0) {
+                result.add(index + 1);
+            } else {
+                arr[index] = -arr[index];
+            }
+        }
+        
+        Collections.sort(result);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        Solution Obj = new Solution();
+        Obj.allDuplicates(n, arr);
+        sc.close();
+    }
+}
