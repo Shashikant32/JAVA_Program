f80b9afc3659af4d9915c666be39d91ff5f45091
shashikant singh <codershashi322@gmail.com>
2024-05-25 23:07:48 +0530

    100DaysCodeChallenge Day:56/100

---
+++ LinkedList/Print in Reverse.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        next = null;
+    }
+}
+class LinkedList{
+    Node head;
+    void add(int data){
+        Node new_node = new Node(data);
+        if(head == null){
+            head = new_node;
+            return;
+        }
+        Node curr = head;
+        while(curr.next != null)
+            curr = curr.next;
+        curr.next = new_node;
+    }
+}
+public class Main {
+    public static void reverse(Node curr){
+      if (curr == null)
+            return;
+        reverse(curr.next);
+        System.out.print(curr.data + " ");
+    }
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+        int n = input.nextInt();
+        for(int i = 0; i < n; i++){
+            int x = input.nextInt();
+            list.add(x);
+        }
+        reverse(list.head);
+        System.out.println("");
+    }
+}
