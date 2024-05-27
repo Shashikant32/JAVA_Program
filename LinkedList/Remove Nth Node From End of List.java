b80560941e166ee0a73c9a310d3083a23e3bfbf5
shashikant singh <codershashi322@gmail.com>
2024-05-27 23:16:58 +0530

    100DaysCodeChallenge Day:58/100

---
+++ LinkedList/Remove Nth Node From End of List.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+}
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+            String S[] = br.readLine().split(" ");
+			int N = Integer.parseInt(S[0]);
+            int n = Integer.parseInt(S[1]);
+    
+		
+		    String S1[] = br.readLine().split(" ");
+			insertion llist = new insertion(); 
+			int a1=Integer.parseInt(S1[0]);
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) 
+			{
+				int a = Integer.parseInt(S1[i]);
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		Node newhead=ob.removeNthFromEnd(llist.head, n);
+		llist.printList(newhead);
+    }
+}
+
+class Solution
+{
+    public static Node removeNthFromEnd(Node head, int n) {
+    Node dummy = new Node(-1);
+        Node fast = dummy;
+        Node slow = dummy;
+        dummy.next = head;
+        for(int i=1; i<=n; i++)
+            {
+                fast = fast.next;
+            }
+        if(fast.next == null)
+        {
+        head = head.next;
+            return head;
+        }
+
+        while(fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+        slow.next =slow.next.next;
+        return head;
+}
+}
+
