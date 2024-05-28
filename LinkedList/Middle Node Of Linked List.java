f9780ab86bc27036c40c4690017c745752b4b422
shashikant singh <codershashi322@gmail.com>
2024-05-28 23:51:12 +0530

    100DaysCodeChallenge Day:59/100

---
+++ LinkedList/Middle Node Of Linked List.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        LinkedList llist= new LinkedList(); 
+        int a1=sc.nextInt();
+        Node head= new Node(a1);
+        llist.addToTheLast(head);
+        for (int i = 1; i < n; i++) 
+        {
+            int a = sc.nextInt(); 
+            llist.addToTheLast(new Node(a));
+        }
+            
+        Solution A = new Solution();
+        llist.head = A.midpointOfLinkedList(llist.head);
+        llist.printList();
+        
+    }
+}
+
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    
+class LinkedList
+{
+    Node head;  
+    Node tail;
+    public void addToTheLast(Node node) 
+    {
+      if (head == null) 
+      {
+       head = node;
+       tail = node;
+      } 
+      else 
+      {
+       tail.next = node;
+       tail = node;
+      }
+    }
+      void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+    
+}
+
+class Solution
+{
+    static Node midpointOfLinkedList(Node head)
+    {
+        Node fast = head;
+        Node slow = head;
+        while(fast != null && fast.next != null)
+
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+        return slow;
+    }
+    
+}
