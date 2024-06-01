ac39317ef69d49c91eee7ef7ede122d3786add6c
shashikant singh <codershashi322@gmail.com>
2024-06-01 22:30:24 +0530

    100DaysCodeChallenge Day:63/100

---
+++ LinkedList/Linked List Cycle.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    Node tail;
+
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public static void makeLoop(Node head, int x) {
+        if (x == 0)
+            return;
+        Node curr = head;
+        Node last = head;
+
+        int currentPosition = 1;
+        while (currentPosition < x) {
+            curr = curr.next;
+            currentPosition++;
+        }
+
+        while (last.next != null)
+            last = last.next;
+        last.next = curr;
+    }
+
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        LinkedList llist = new LinkedList();
+        int a1 = sc.nextInt();
+        Node head = new Node(a1);
+        llist.addToTheLast(head);
+        for (int i = 1; i < n; i++) {
+            int a = sc.nextInt();
+            llist.addToTheLast(new Node(a));
+        }
+
+        int pos = sc.nextInt();
+        llist.makeLoop(head, pos);
+
+        Solution x = new Solution();
+        boolean a = x.detectLoop(head);
+
+        if (a == true)
+            System.out.println(1);
+        else
+            System.out.println(0);
+
+    }
+}
+
+class Solution {
+    public static boolean detectLoop(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
