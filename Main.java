c56a7afc321b1223eb531b49befd46ade65bfdd8
shashikant singh <codershashi322@gmail.com>
2024-06-16 22:09:38 +0530

    100DaysCodeChallenge Day:78/100

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        char ch = scr.next().charAt(0);
+        int result=0;
+        if(ch>='A' && ch<='Z')  
+        {
+            result=1;
+        }else if(ch>= 'a' && ch<='z') 
+        {
+            result =0;
+        }else               
+        {
+            result =-1;
+        }
+        System.out.println(result);
+    }
+}
