ceb696ef3333042605f13b114bbb1adf6120176c
shashikant singh <codershashi322@gmail.com>
2024-06-22 21:15:43 +0530

    100DaysCodeChallenge Day:84/100

---
+++ HashMap/First Element to Occur K Times.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public void firstElementToOccurKTimes(int[] num, int n, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++)
+            {
+                if(map.containsKey(num[i]))
+                {
+                    int currfq= map.get(num[i]);
+                    map.put(num[i],currfq+1);
+                }else
+                {
+                    map.put(num[i],1);
+                }
+                if(map.get(num[i])==k)
+                {
+                    System.out.println(num[i]);
+                    return;
+                }
+            }
+        System.out.println(-1);
+        
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n,k;
+        n=sc.nextInt();
+        k=sc.nextInt();
+        int nums[] = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        Solution Obj = new Solution();
+        Obj.firstElementToOccurKTimes(nums,n,k);  
+        System.out.println();
+    }
+}
