4122bc0afe78a7a9c0291e423e77e6296dfa4bc8
shashikant singh <codershashi322@gmail.com>
2024-06-25 23:24:25 +0530

    100DaysCodeChallenge Day:87/100

---
+++ HashMap/Most Frequent Word in Array.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public void mostFrequent(String[] arr, int n) {
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        Map<String, Integer> lastOccurrenceIndex = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            String word = arr[i];
+            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+            lastOccurrenceIndex.put(word, i);
+        }
+        
+        String mostFrequentWord = "";
+        int maxFrequency = 0;
+        int maxLastOccurrenceIndex = -1;
+        
+        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
+            String word = entry.getKey();
+            int frequency = entry.getValue();
+            int lastIdx = lastOccurrenceIndex.get(word);
+            
+            if (frequency > maxFrequency || (frequency == maxFrequency && lastIdx > maxLastOccurrenceIndex)) {
+                maxFrequency = frequency;
+                maxLastOccurrenceIndex = lastIdx;
+                mostFrequentWord = word;
+            }
+        }
+        
+        System.out.println(mostFrequentWord);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+        
+        Solution obj = new Solution();
+        obj.mostFrequent(arr, n);
+        
+        sc.close();
+    }
+}
