f9a2ebfe83a703038f85207438980248d162267f
shashikant singh <codershashi322@gmail.com>
2024-07-04 23:22:27 +0530

    100DaysCodeChallenge Day:96/100

---
+++ Trees/Diameter of a Binary Tree.java
@@ -0,0 +1,80 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if (!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if (i >= ip.length) break;
+            currVal = ip[i];
+            if (!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        Node root1 = buildTree(s1);
+        Solution g = new Solution();
+        System.out.println(g.diameter(root1));
+    }
+}
+
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+    public int diameter(Node root) {
+        if (root == null)
+            return 0;
+        
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int throughRoot = leftHeight + rightHeight + 1;
+        
+        int leftDiameter = diameter(root.left);
+        int rightDiameter = diameter(root.right);
+        
+        return Math.max(throughRoot, Math.max(leftDiameter, rightDiameter));
+    }
+    
+    private int height(Node node) {
+        if (node == null)
+            return 0;
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+}
