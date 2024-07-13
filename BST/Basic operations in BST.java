3d29c8ed8265afc841f5f3ae65cca434f260cdbf
shashikant singh <codershashi322@gmail.com>
2024-07-13 14:34:29 +0530

    Basic operations in BST

---
+++ BST/Basic operations in BST.java
@@ -0,0 +1,175 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static int size(Node node) {
+      //time complx-->O(n), SC--> O(h)---->O(N)
+   if (node == null) {
+        return 0;
+    }
+    return 1 + size(node.left) + size(node.right);
+  }
+
+  public static int sum(Node node) {
+            //time complx-->O(n), SC--> O(h)---->O(N)
+     if (node == null) {
+        return 0;
+    }
+    return node.data + sum(node.left) + sum(node.right);
+  }
+
+  public static int max(Node node) {
+           //time complx-->O(n), SC--> O(h)---->O(N)
+    if (node == null) {
+        return Integer.MIN_VALUE;
+    }
+    int leftMax = max(node.left);
+    int rightMax = max(node.right);
+    return Math.max(node.data, Math.max(leftMax, rightMax));
+
+      // if(node == null) return -1;
+      // Node cur = node;
+      // while(cur.left!=null)
+      //     {
+      //         cur=cur.right;
+      //     }
+      // return cur.data;
+  }
+
+  public static int min(Node node) {
+           //time complx-->O(n), SC--> O(h)---->O(N)
+    if (node == null) {
+        return Integer.MAX_VALUE;
+    }
+    int leftMin = min(node.left);
+    int rightMin = min(node.right);
+    return Math.min(node.data, Math.min(leftMin, rightMin));
+
+      // if(node == null) return -1;
+      // Node cur = node;
+      // while(cur.left!=null)
+      //     {
+      //         cur=cur.left;
+      //     }
+      // return cur.data;
+  }
+
+  public static boolean find(Node node, int data){
+           //time complx-->O(n), SC--> O(h)---->O(N) 
+   if (node == null) {
+        return false;
+    }
+    if (node.data == data) {
+        return true;
+    }
+    return find(node.left, data) || find(node.right, data);
+  }  
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    int data = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+
+    int size = size(root);
+    int sum = sum(root);
+    int max = max(root);
+    int min = min(root);
+    boolean found = find(root, data);
+
+    System.out.println(size);
+    System.out.println(sum);
+    System.out.println(max);
+    System.out.println(min);
+    System.out.println(found);
+  }
+
+}
