90e39563fe92f9eda3427b6ea5550752367c300b
shashikant singh <codershashi322@gmail.com>
2024-07-24 23:16:51 +0530

    Update Maximum Depth of Binary Tree.java

--- BST/Maximum Depth of Binary Tree.java
+++ BST/Maximum Depth of Binary Tree.java
@@ -72,12 +72,10 @@ class Main {
 }
 
 class Solution {
-    // Method to find the height of the binary tree
     int height(Node node) {
         if (node == null) {
             return 0;
         }
-        // Compute the depth of each subtree and add 1 for the root
         int leftHeight = height(node.left);
         int rightHeight = height(node.right);
         return Math.max(leftHeight, rightHeight) + 1;
