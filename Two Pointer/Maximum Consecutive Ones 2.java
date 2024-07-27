67308e4e1d675b49116642911f75392a75cd4824
shashikant singh <codershashi322@gmail.com>
2024-07-27 23:32:01 +0530

    Update Maximum Consecutive Ones 2.java

--- Two Pointer/Maximum Consecutive Ones 2.java
+++ Two Pointer/Maximum Consecutive Ones 2.java
@@ -1,3 +1,40 @@
-public class Maximum Consecutive Ones 2 {
-    
+import java.util.*;
+
+class Solution {
+    static int maxOne(int arr[], int n,int k)
+    {
+        int l = 0, r= 1, ans=0;
+        if(arr[l]==0) k--;
+        while(r<n)
+            {
+                while(k<=0 && arr[r]==0)
+                    {
+                        if(arr[l]==0)
+                        {
+                            k++;
+                        }
+                        l++;
+                    }
+                if(arr[r]== 0) k--;
+                ans = Math.max(r-l+1,ans);
+                r++;
+            }
+        return ans;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int k= sc.nextInt();
+        int array[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            array[i]= sc.nextInt();
+        }
+        Solution Obj = new Solution();
+        System.out.println(Obj.maxOne(array,n,k));
+    }
 }
