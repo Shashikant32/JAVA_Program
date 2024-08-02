0e57711e15a39b67db0e4d9cf04e656ead8b6376
shashikant singh <codershashi322@gmail.com>
2024-08-02 23:28:28 +0530

    Bottom view of Binary tree

--- BST/Right view of Binary tree.java
+++ BST/Right view of Binary tree.java
@@ -1,3 +1,104 @@
-public class Right view of Binary tree {
-    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException; // Import IOException
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Main {
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String[] ip = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+
+        while (queue.size() > 0 && i < ip.length) {
+            Node currNode = queue.poll();
+            String currVal = ip[i];
+
+            if (!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if (i >= ip.length) break;
+            currVal = ip[i];
+
+            if (!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution tree = new Solution();
+            ArrayList<Integer> arr = tree.rightView(root);
+            for (int x : arr) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution {
+    ArrayList<Integer> rightView(Node root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        // Queue for level order traversal
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            // Traverse all nodes at the current level
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
+
+                // If it's the last node in this level, add to result
+                if (i == size - 1) {
+                    result.add(current.data);
+                }
+
+                // Add left and right children to the queue
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+        }
+
+        return result;
+    }
 }
