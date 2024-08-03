b4fe23e9ddec3ead16be2cabd81734e3d677d8fb
shashikant singh <codershashi322@gmail.com>
2024-08-03 13:33:13 +0530

    update time and space complx..

--- GRAPH/DFS Implementation.java
+++ GRAPH/DFS Implementation.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
 class Solution {
-   
+  // O(V+E log E)--> TC
+    // SC--> O(N)
     public static void DFSTraversal(List<List<Integer>> edges, int n) {
         
         // first create adj list
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<Integer>());
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();// O(2E)
+        for(int i=0; i<n; i++) adj.add(new ArrayList<Integer>());// O (N)
         int E = edges.size();
-        for(int i=0; i<E; i++)
+        for(int i=0; i<E; i++)// O(E)
             {
                 int u=edges.get(i).get(0);
                 int v=edges.get(i).get(1);
@@ -16,11 +17,12 @@ class Solution {
                 adj.get(v).add(u);
             
             }
-        for(int i=0; i<n; i++) Collections.sort(adj.get(i));
+        for(int i=0; i<n; i++) Collections.sort(adj.get(i));//O(N) + O(E log E)
         // mann dfs start
-        int [] vis = new int[n];
+        int [] vis = new int[n];//O(N)
         dfs(adj,0,vis);
     }
+     // O(N) + O(2E) --> TC --.(V+E)
      public static void dfs(ArrayList<ArrayList<Integer>> adj, int node, int[] vis)
     {
         //visit this node
