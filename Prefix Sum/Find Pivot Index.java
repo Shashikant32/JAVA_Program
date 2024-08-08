3b0c0f33f2b41d8b9dca52bb03f68553ab1da0c4
shashikant singh <codershashi322@gmail.com>
2024-08-08 22:33:36 +0530

    Create Find Pivot Index.java

---
+++ Prefix Sum/Find Pivot Index.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        int leftSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            
+            if (leftSum == rightSum) {
+                return i; 
+            }
+            
+            leftSum += nums[i];
+        }
+        
+        return -1; 
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        Solution Obj = new Solution();
+        int result = Obj.pivotIndex(arr);
+        System.out.println(result);
+        sc.close();
+    }
+}
