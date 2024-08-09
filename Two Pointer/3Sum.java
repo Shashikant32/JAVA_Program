117f33ca8ce62198eb3ee7d804d7db133720068e
shashikant singh <codershashi322@gmail.com>
2024-08-09 23:00:03 +0530

    3Sum

---
+++ Two Pointer/3Sum.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = input.nextInt();
+        }
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.threeSum(nums);
+        // Sort all lists inside the list first
+        for (List<Integer> list : result) {
+            Collections.sort(list);
+        }
+        // Sort the list of lists
+        Collections.sort(result, new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> l1, List<Integer> l2) {
+                for (int i = 0; i < l1.size(); i++) {
+                    if (l1.get(i) < l2.get(i)) return -1;
+                    else if (l1.get(i) > l2.get(i)) return 1;
+                }
+                return 0;
+            }
+        });
+        for (List<Integer> triplet : result) {
+            for (int num : triplet) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // Skip duplicates for the first element
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // Skip duplicates for the second element
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    // Skip duplicates for the third element
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
