5be8bf9c3b6d8cea0bad5d43b866518c56d11044
shashikant singh <codershashi322@gmail.com>
2024-08-14 13:34:03 +0530

    Symmetric Tree

---
+++ Trees/Symmetric Tree.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+    public static void isSymmetrical(Node root) {
+        if (root == null) {
+            System.out.println("YES");
+            return;
+        }
+        if (isTreeSymmetrical(root.left, root.right)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    static boolean isTreeSymmetrical(Node left, Node right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.data != right.data) {
+            return false;
+        }
+        return isTreeSymmetrical(left.left, right.right) && isTreeSymmetrical(left.right, right.left);
+    }
+}
+
+class Main {
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+        String[] ip = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+            Node currNode = queue.poll();
+            String currVal = ip[i];
+            if (!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if (i >= ip.length) break;
+            currVal = ip[i];
+            if (!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        Node root1 = buildTree(s1);
+        Solution g = new Solution();
+        g.isSymmetrical(root1);
+    }
+}
