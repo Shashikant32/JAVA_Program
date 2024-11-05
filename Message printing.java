d0f727990d0f2f9cb13c35c0b589a80d19ff358a
shashikant singh <codershashi322@gmail.com>
2024-11-05 18:33:36 +0530

    Create Message printing.java

---
+++ Message printing.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner scanner = new Scanner(System.in);
+        
+        String str = scanner.nextLine();
+        System.out.print("My name is "+ str +". I like to code.");
+    }
+}
